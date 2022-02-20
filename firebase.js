64931235e1642119cba3ca4b5da0e64a9a39485f
1homsi <65892600+1homsi@users.noreply.github.com>
2022-02-20 13:56:15 +0200

    Error Fixed

---
+++ firebase.js
@@ -0,0 +1,21 @@
+import * as firebase from "firebase";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyC7ibM_dWnupDYpIbTX2c0lfYAXZrpTH7k",
+    authDomain: "quakly-ca6c2.firebaseapp.com",
+    projectId: "quakly-ca6c2",
+    storageBucket: "quakly-ca6c2.appspot.com",
+    messagingSenderId: "815018900166",
+    appId: "1:815018900166:web:14f8822da3a289640275c7"
+};
+
+let app;
+if (firebase.apps.length === 0) {
+    app = firebase.initializeApp(firebaseConfig);
+} else {
+    app = firebase.app()
+}
+
+const auth = firebase.auth()
+
+export { auth };
