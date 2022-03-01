3cb0a1978816fe13340f2d9084707ed750a491e4
1homsi <65892600+1homsi@users.noreply.github.com>
2022-03-01 09:52:19 +0200

    Added Add product screen

--- Data.js
+++ Data.js
@@ -63,6 +63,162 @@ const Data = [
         title: "Data",
         dis: "..."
     },
+    {
+        title: "Data",
+        dis: "..."
+    },
+    {
+        title: "Data",
+        dis: "..."
+    },
+    {
+        title: "Data",
+        dis: "..."
+    },
+    {
+        title: "Data",
+        dis: "..."
+    },
+    {
+        title: "Data",
+        dis: "..."
+    },
+    {
+        title: "Data",
+        dis: "..."
+    },
+    {
+        title: "Data",
+        dis: "..."
+    },
+    {
+        title: "Data",
+        dis: "..."
+    },
+    {
+        title: "Data",
+        dis: "..."
+    },
+    {
+        title: "Data",
+        dis: "..."
+    },
+    {
+        title: "Data",
+        dis: "..."
+    },
+    {
+        title: "Data",
+        dis: "..."
+    },
+    {
+        title: "Data",
+        dis: "..."
+    },
+    {
+        title: "Data",
+        dis: "..."
+    },
+    {
+        title: "Data",
+        dis: "..."
+    },
+    {
+        title: "Data",
+        dis: "..."
+    },
+    {
+        title: "Data",
+        dis: "..."
+    },
+    {
+        title: "Data",
+        dis: "..."
+    },
+    {
+        title: "Data",
+        dis: "..."
+    },
+    {
+        title: "Data",
+        dis: "..."
+    },
+    {
+        title: "Data",
+        dis: "..."
+    },
+    {
+        title: "Data",
+        dis: "..."
+    },
+    {
+        title: "Data",
+        dis: "..."
+    },
+    {
+        title: "Data",
+        dis: "..."
+    },
+    {
+        title: "Data",
+        dis: "..."
+    },
+    {
+        title: "Data",
+        dis: "..."
+    },
+    {
+        title: "Data",
+        dis: "..."
+    },
+    {
+        title: "Data",
+        dis: "..."
+    },
+    {
+        title: "Data",
+        dis: "..."
+    },
+    {
+        title: "Data",
+        dis: "..."
+    },
+    {
+        title: "Data",
+        dis: "..."
+    },
+    {
+        title: "Data",
+        dis: "..."
+    },
+    {
+        title: "Data",
+        dis: "..."
+    },
+    {
+        title: "Data",
+        dis: "..."
+    },
+    {
+        title: "Data",
+        dis: "..."
+    },
+    {
+        title: "Data",
+        dis: "..."
+    },
+    {
+        title: "Data",
+        dis: "..."
+    },
+    {
+        title: "Data",
+        dis: "..."
+    },
+    {
+        title: "Data",
+        dis: "..."
+    },
 ]
 
 export default Data
--- screens/AddProduct.js
+++ screens/AddProduct.js
@@ -1,10 +1,20 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import { auth } from "../firebase"
 
 const AddProduct = () => {
+    const navigation = useNavigation()
     return (
         <View>
-            <Text>AddProduct</Text>
+            {!auth.currentUser ?
+                <>
+                    {navigation.replace("Home")}
+                </>
+                :
+                <>
+                    <Text>Hello world</Text>
+                </>
+            }
         </View>
     )
 }
