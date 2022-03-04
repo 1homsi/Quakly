bfcf81c905cdb2510b4d013bdb98125332488a38
alifouani1 <fouaniali08@gmail.com>
2022-03-04 23:00:51 +0200

    Update Options.js

--- screens/Options.js
+++ screens/Options.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { StyleSheet, View, Text, TouchableOpacity, Alert } from "react-native";
+import { SafeAreaView, StyleSheet, View, Text, TouchableOpacity, Alert } from "react-native";
 import { auth } from "../firebase";
 import { useNavigation } from "@react-navigation/native";
 
@@ -49,29 +49,76 @@ export default function Option() {
         );
 
     return (
+        <SafeAreaView style={styles.bigMain}>
+
+        <View style={styles.topNav}>
+            <Text style={styles.title}>Reset Password</Text>
+        </View>
         <View style={styles.container}>
-            <Text>Email: {auth.currentUser?.email}</Text>
-            <TouchableOpacity
+            <Text style={styles.emailSec}>Email: {auth.currentUser?.email}</Text>
+            <TouchableOpacity   
                 onPress={handleSignOut}
-                style={styles.button}
+                style={styles.button1}
             >
                 <Text style={styles.buttonText}>Sign out</Text>
             </TouchableOpacity>
             <TouchableOpacity
                 onPress={handleDeleteUser}
-                style={styles.button}
+                style={styles.button2}
             >
                 <Text style={styles.buttonText}>Delete Account</Text>
             </TouchableOpacity>
         </View>
+        </SafeAreaView>
     );
 }
 
 const styles = StyleSheet.create({
+    bigMain:{
+        flex:1,
+        backgroundColor: '#fff'
+    },
+    topNav:{
+        backgroundColor: "darkblue",
+        paddingTop: 60
+    },
     container: {
+        paddingBottom: "80%",
         flex: 1,
         backgroundColor: "white",
         alignItems: "center",
         justifyContent: "center",
-    }
+    },
+    title: {
+        marginLeft: 10,
+        marginBottom:40,
+        color: 'white',
+        fontSize: 25,
+        fontWeight: "bold"        
+    },
+    emailSec: {
+        fontSize: 22
+    },
+    button1: {
+        backgroundColor: '#89CFF0',
+        width: '100%',
+        padding: 20,
+        borderRadius: 30,
+        alignItems: 'center',
+        width: '80%',
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginTop: 20,
+    },
+    button2: {
+        backgroundColor: 'red',
+        width: '100%',
+        padding: 20,
+        borderRadius: 30,
+        alignItems: 'center',
+        width: '80%',
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginTop: 20,
+    },
 });
