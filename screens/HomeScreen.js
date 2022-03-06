88cc6507db1ffd1c289c8ae6f2bbef9c61848226
alifouani1 <fouaniali08@gmail.com>
2022-03-06 17:17:41 +0200

    Update HomeScreen.js

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -14,82 +14,45 @@ import { auth, db } from "../firebase";
 import { Icon } from "react-native-elements";
 
 const wait = (timeout) => {
-  return new Promise(resolve => setTimeout(resolve, timeout));
-}
+  return new Promise((resolve) => setTimeout(resolve, timeout));
+};
 
 const HomeScreen = () => {
   const navigation = useNavigation();
-  const [data, setData] = React.useState([])
+  const [data, setData] = React.useState([]);
   const [refreshing, setRefreshing] = React.useState(false);
 
   const fetchAll = () => {
-    db.collection("Product").where("ProductTaken", "!=", true)
+    db.collection("Product")
+      .where("ProductTaken", "!=", true)
       .get()
       .then((querySnapshot) => {
         querySnapshot.forEach((doc) => {
-          let Userdata = Object.assign({ id: doc.id }, doc.data())
-          setData(e => [...e, Userdata]);
+          let Userdata = Object.assign({ id: doc.id }, doc.data());
+          setData((e) => [...e, Userdata]);
         });
       });
-  }
+  };
 
   React.useEffect(() => {
-    fetchAll()
+    fetchAll();
   }, []);
-
-
-
 
   const onRefresh = React.useCallback(() => {
     setRefreshing(true);
     setData([]);
-    fetchAll()
+    fetchAll();
     wait(2000).then(() => setRefreshing(false));
   }, []);
 
   return (
     <SafeAreaView style={styles.container}>
       <View style={styles.Top}>
-        {!auth.currentUser ? (
-          <>
-            <Text
-              onPress={() => {
-                navigation.replace("Login");
-              }}
-            >
-              lol
-            </Text>
-          </>
-        ) : (
-          <>
-            <TouchableOpacity style={styles.ProfileDot}>
-              <Icon
-                style={styles.icon}
-                reverseColor
-                name="user"
-                type="font-awesome"
-                size={35}
-                onPress={() => navigation.navigate("Option")}
-              />
-            </TouchableOpacity>
-          </>
-        )}
-
         <View style={{ flex: 1 }}>
           <Text placeholder="Test" style={styles.HeadTitlte}>
             Quakly
           </Text>
         </View>
-        <TouchableOpacity style={styles.addProd}>
-          <Icon
-            style={styles.iconAdd}
-            reverseColor
-            name="plus"
-            type="font-awesome"
-            size={36}
-            onPress={() => navigation.replace("AddProduct")}
-          />
-        </TouchableOpacity>
       </View>
       <View style={styles.ListView}>
         <FlatList
@@ -102,9 +65,59 @@ const HomeScreen = () => {
           }
           style={styles.list}
           data={data}
-          renderItem={({ item }) => <Items title={item.title} dis={item.Description} />}
+          renderItem={({ item }) => (
+            <Items title={item.title} dis={item.Description} />
+          )}
           keyExtractor={(item, index) => index.toString()}
         />
+      </View>
+      <View style={styles.bottomNav}>
+        <TouchableOpacity style={styles.bottomNavItem}>
+          <Icon
+            style={styles.icon}
+            reverseColor
+            name="home"
+            type="font-awesome"
+            size={35}
+            onPress={() => navigation.replace("Home")}
+          />
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.bottomNavItemAdd}>
+          <Icon
+            style={styles.icon}
+            reverseColor
+            name="plus"
+            type="font-awesome"
+            size={35}
+            onPress={() => navigation.replace("AddProduct")}
+          />
+        </TouchableOpacity>
+        <View>
+          {!auth.currentUser ? (
+            <>
+              <Text
+                onPress={() => {
+                  navigation.replace("Login");
+                }}
+              >
+                lol
+              </Text>
+            </>
+          ) : (
+            <>
+              <TouchableOpacity style={styles.bottomNavItem}>
+                <Icon
+                  style={styles.icon}
+                  reverseColor
+                  name="user"
+                  type="font-awesome"
+                  size={35}
+                  onPress={() => navigation.navigate("Option")}
+                />
+              </TouchableOpacity>
+            </>
+          )}
+        </View>
       </View>
     </SafeAreaView>
   );
@@ -119,14 +132,6 @@ const styles = StyleSheet.create({
     fontWeight: "800",
     color: "#000",
   },
-  icon: {
-    marginRight: "10%",
-    color: "#003f5c",
-  },
-  ProfileDot: {
-    paddingLeft: "3%",
-    paddingTop: "0.5%",
-  },
   Top: {
     marginTop: 20,
     flexDirection: "row",
@@ -135,7 +140,6 @@ const styles = StyleSheet.create({
     marginTop: 40,
     paddingBottom: 10,
     paddingTop: 10,
-    // backgroundColor: "#fc5c65",
   },
   ListView: {
     width: "100%",
@@ -152,24 +156,33 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     alignItems: "center",
   },
-  button: {
-    backgroundColor: "#0782F9",
-    width: "60%",
-    padding: 15,
-    borderRadius: 10,
+  bottomNav: {
+    flexDirection: "row",
+    justifyContent: "space-around",
     alignItems: "center",
-    marginTop: 30,
+    backgroundColor: "white",
+    height: "10%",
+    width: "100%",
+    position: "absolute",
+    bottom: 0,
   },
-  buttonText: {
-    color: "white",
-    fontWeight: "700",
-    fontSize: 16,
+  bottomNavItem: {
+    alignItems: "center",
+    justifyContent: "center",
+    color: "black",
   },
-  iconAdd: {
-    color: "#003f5c",
-  },
-  addProd: {
-    paddingRight: "3%",
-    paddingTop: "0.8%",
+  bottomNavItemAdd: {
+    color: "black",
+    zIndex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#fc5c65",
+    borderColor: "white",
+    borderWidth: 8,
+    paddingTop: 35,
+    paddingHorizontal: 21,
+    paddingBottom: 35,
+    borderRadius: 100,
+    marginBottom: 40,
   },
 });
