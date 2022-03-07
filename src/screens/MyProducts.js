96058de0e086d5b30c398cef0456a175a9b24870
alifouani1 <fouaniali08@gmail.com>
2022-03-07 23:12:12 +0200

    styling

--- src/screens/MyProducts.js
+++ src/screens/MyProducts.js
@@ -1,109 +1,196 @@
-import { View, FlatList, StyleSheet, SafeAreaView, Text, Platform, Linking } from 'react-native'
-import React from 'react'
-import { db, auth } from '../../firebase'
-import { useNavigation } from '@react-navigation/native'
-import BottonNav from '../components/BottomNav'
+import {
+  View,
+  FlatList,
+  StyleSheet,
+  SafeAreaView,
+  Text,
+  Platform,
+  Linking,
+} from "react-native";
+import React from "react";
+import { db, auth } from "../../firebase";
+import { useNavigation } from "@react-navigation/native";
+import BottomNav from "../components/BottomNav";
 import LottieView from "lottie-react-native";
 
 const MyProducts = () => {
-    const navigation = useNavigation();
-    const [data, setData] = React.useState([])
-    const [loading, setLoading] = React.useState(true)
+  const navigation = useNavigation();
+  const [data, setData] = React.useState([]);
+  const [loading, setLoading] = React.useState(true);
 
-    React.useEffect(() => {
-        if (!auth.currentUser) {
-            navigation.replace("Login")
-        }
+  React.useEffect(() => {
+    if (!auth.currentUser) {
+      navigation.replace("Login");
+    }
 
-
-        if (auth.currentUser) {
-            db.collection("Product").where("Email", "==", auth.currentUser?.email)
-                .get()
-                .then((querySnapshot) => {
-                    setLoading(false)
-                    querySnapshot.forEach((doc) => {
-                        // console.log(Object.assign({ id: doc.id }, doc.data()));
-                        let Userdata = Object.assign({ id: doc.id }, doc.data())
-                        setData(e => [...e, Userdata]);
-                    });
-                });
-        }
-    }, [])
-
-    const openMaps = (lat, lng) => {
-        const scheme = Platform.select({ ios: 'maps:0,0?q=', android: 'geo:0,0?q=' });
-        const latLng = `${lat},${lng}`;
-        const label = 'Custom Label';
-        const url = Platform.select({
-            ios: `${scheme}${label}@${latLng}`,
-            android: `${scheme}${latLng}(${label})`
+    if (auth.currentUser) {
+      db.collection("Product")
+        .where("Email", "==", auth.currentUser?.email)
+        .get()
+        .then((querySnapshot) => {
+          setLoading(false);
+          querySnapshot.forEach((doc) => {
+            // console.log(Object.assign({ id: doc.id }, doc.data()));
+            let Userdata = Object.assign({ id: doc.id }, doc.data());
+            setData((e) => [...e, Userdata]);
+          });
         });
-        Linking.openURL(url);
     }
+  }, []);
 
-    const handleDelete = (item) => {
-        // console.log(item.id)
-        db.collection("Product").doc(item.id).delete()
-        //TODO: remove product from state array
-        navigation.replace("Home")
-    }
+  const openMaps = (lat, lng) => {
+    const scheme = Platform.select({
+      ios: "maps:0,0?q=",
+      android: "geo:0,0?q=",
+    });
+    const latLng = `${lat},${lng}`;
+    const label = "Custom Label";
+    const url = Platform.select({
+      ios: `${scheme}${label}@${latLng}`,
+      android: `${scheme}${latLng}(${label})`,
+    });
+    Linking.openURL(url);
+  };
 
-    const Item = (props) => (
-        <View style={styles.item}>
-            <Text style={styles.description}>{props.id}</Text>
-            <Text onPress={() => { openMaps(props.Location.coords.latitude, props.Location.coords.longitude) }}>Open Location</Text>
-            <Text onPress={() => handleDelete(props.item)}>Delete item</Text>
-        </View >
-    );
+  const handleDelete = (item) => {
+    // console.log(item.id)
+    db.collection("Product").doc(item.id).delete();
+    //TODO: remove product from state array
+    navigation.replace("Home");
+  };
 
-    const renderItem = ({ item }) => (
-        <Item id={item.id} Location={item.Location} Delete={item.id} item={item} />
-    );
+  const Item = (props) => (
+    <View style={styles.item}>
+      <Text style={styles.description}>{props.id}</Text>
+      <Text
+        style={styles.location}
+        onPress={() => {
+          openMaps(
+            props.Location.coords.latitude,
+            props.Location.coords.longitude
+          );
+        }}
+      >
+        Open Location
+      </Text>
+      <Text style={styles.delete} onPress={() => handleDelete(props.item)}>
+        Delete item
+      </Text>
+      <Text style={styles.edit}>Edit Product</Text>
+    </View>
+  );
 
-    return (
-        <SafeAreaView style={styles.container}>
-            {loading ?
-                <>
-                    <LottieView
-                        source={require("../../assets/88404-loading-bubbles.json")}
-                        style={styles.animation}
-                        autoPlay
-                    />
-                </>
-                :
-                <>
-                    {data.length > 0 ?
-                        <FlatList
-                            data={data}
-                            renderItem={renderItem}
-                            keyExtractor={(item, index) => index.toString()}
-                        />
-                        :
-                        <Text>No Products</Text>
-                    }
-                </>
-            }
-        </SafeAreaView>
-    )
-}
+  const renderItem = ({ item }) => (
+    <Item id={item.id} Location={item.Location} Delete={item.id} item={item} />
+  );
 
-export default MyProducts
+  return (
+    <SafeAreaView style={styles.container}>
+      {loading ? (
+        <>
+          <LottieView
+            source={require("../../assets/88404-loading-bubbles.json")}
+            style={styles.animation}
+            autoPlay
+          />
+        </>
+      ) : (
+        <>
+          {data.length > 0 ? (
+            <View style={styles.ListView}>
+              <View style={styles.Top}>
+                <View style={{ flex: 1 }}>
+                  <Text placeholder="Test" style={styles.HeadTitlte}>
+                    My Products
+                  </Text>
+                </View>
+              </View>
+              <FlatList
+                style={styles.list}
+                data={data}
+                renderItem={renderItem}
+                keyExtractor={(item, index) => index.toString()}
+              />
+            </View>
+          ) : (
+            <Text>No Products</Text>
+          )}
+        </>
+      )}
+      <BottomNav />
+    </SafeAreaView>
+  );
+};
+
+export default MyProducts;
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-    },
-    item: {
-        backgroundColor: '#D1C7C7',
-        padding: 20,
-        marginVertical: 8,
-        marginHorizontal: 16,
-    },
-    title: {
-        fontSize: 32,
-    },
-    animation: {
-        width: "100%",
-        height: "90%",
-    },
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  item: {
+    backgroundColor: "#dba0a3",
+    padding: 20,
+    marginVertical: 8,
+    marginHorizontal: 16,
+    borderRadius: 10,
+  },
+  title: {
+    fontSize: 32,
+  },
+  animation: {
+    width: "100%",
+    height: "90%",
+  },
+  ListView: {
+    width: "100%",
+    alignContent: "center",
+    justifyContent: "center",
+    flex: 1,
+    // marginTop: 50,
+
+    // marginLeft: "15%",
+  },
+  list: {
+    width: "100%",
+    borderRadius: 50,
+    borderColor: "black",
+  },
+  HeadTitlte: {
+    marginLeft: "5%",
+    fontSize: 40,
+    fontWeight: "800",
+    color: "#000",
+  },
+  Top: {
+    marginTop: 20,
+    flexDirection: "row",
+    textAlign: "center",
+    marginBottom: 12,
+    marginTop: 40,
+    paddingBottom: 10,
+    paddingTop: 10,
+  },
+  description: {
+    fontSize: 20,
+    color: "#000",
+    marginBottom: 10,
+  },
+  location: {
+    fontSize: 20,
+    color: "#000",
+    marginBottom: 10,
+  },
+  delete: {
+    fontSize: 20,
+    color: "#000",
+    marginBottom: 10,
+  },
+  edit: {
+    fontSize: 20,
+    color: "#000",
+    marginBottom: 10,
+  },
 });
--- src/screens/TermsAndConditions.js
+++ src/screens/TermsAndConditions.js
@@ -4,11 +4,61 @@ import React from 'react'
 const TermsAndConditions = () => {
     return (
         <SafeAreaView>
-            <Text>TermsAndConditions</Text>
+            <View style={styles.Top}>
+                <View style={{ flex: 1 }}>
+                  <Text placeholder="Test" style={styles.HeadTitlte}>
+                    Terms&Conditions
+                  </Text>
+                </View>
+              </View>
+              <View style={styles.main}>
+                <Text style={styles.text}>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                </Text>
+              </View>
         </SafeAreaView>
     )
 }
 
 export default TermsAndConditions
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    Top: {
+        marginTop: 20,
+        flexDirection: "row",
+        textAlign: "center",
+        marginBottom: 12,
+        marginTop: 40,
+        paddingBottom: 10,
+        paddingTop: 10,
+      },
+      HeadTitlte: {
+        marginLeft: "5%",
+        fontSize: 40,
+        fontWeight: "800",
+        color: "#000",
+      },
+        main: {
+            marginTop: 10,
+            flexDirection: "row",
+            textAlign: "center",
+            marginBottom: 12,
+            marginTop: 30,
+            paddingBottom: 30,
+            paddingTop: 20,
+            marginLeft: "7%",
+            marginRight: "7%",
+            borderBottomLeftRadius: 25,
+            borderBottomRightRadius: 25,
+            borderTopRightRadius: 25,
+            backgroundColor: "#fff",
+        },
+        text: {
+            marginLeft: "5%",
+            fontSize: 20,
+            fontWeight: "800",
+            color: "#000",
+            width: "auto",
+            height: "auto",
+        },
+})
